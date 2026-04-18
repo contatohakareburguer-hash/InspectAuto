e9acae7a937b50124c1e48b3b1a48ebecc049bb1
contatohakareburguer-hash <contatohakareburguer@gmail.com>
2026-04-17 22:49:17 -0300

    Add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>Inspect Auto - Inspeção Veícular Profissional</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
